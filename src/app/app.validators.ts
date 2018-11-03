28babd6e69ac8c2855e5df49f1618ac6780b9df2
Nick Hodges <nickhodges@gmail.com>
2018-11-03 07:41:02 -0400

    Remove unnecessary check in validator

--- src/app/app.validators.ts
+++ src/app/app.validators.ts
@@ -3,11 +3,6 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 export class NumberValidators {
   static phone(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
-      if (control.pristine) {
-        return {
-          phone: true
-        };
-      }
       const PHONE_REGEXP = /^\(?([0-9]{3})\)?[-.]?([0-9]{3})[-.]?([0-9]{4})$/;
       control.markAsTouched();
       if (PHONE_REGEXP.test(control.value)) {
