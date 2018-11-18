d5e9976448d130a57e8132343519e4fa0a62ab71
Nick Hodges <nickhodges@gmail.com>
2018-11-18 08:49:48 -0500

    Don't need extra length check

--- src/app/app.validators.ts
+++ src/app/app.validators.ts
@@ -4,7 +4,7 @@ export class NumberValidators {
   static phone(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
       const PHONE_REGEXP = /^\(?([0-9]{3})\)?[-.]?([0-9]{3})[-.]?([0-9]{4})$/;
-      if (PHONE_REGEXP.test(control.value) || control.value.length === 0) {
+      if (PHONE_REGEXP.test(control.value)) {
         return null;
       }
       return {
